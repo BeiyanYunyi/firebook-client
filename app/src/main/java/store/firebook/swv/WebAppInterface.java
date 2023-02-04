eecae7a5c14459265ff737e41cc125adbe31122f
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2023-02-04 14:30:07 +0800

    ✨ Get app version

--- app/src/main/java/store/firebook/swv/WebAppInterface.java
+++ app/src/main/java/store/firebook/swv/WebAppInterface.java
@@ -15,4 +15,9 @@ public class WebAppInterface {
 	public void setCurrUrl(String newUrl) {
 		this.currUrl = newUrl;
 	}
+
+	@JavascriptInterface
+	public String getAppVersion() {
+		return "1.0.0";
+	}
 }
